80dd21a6dc44dc94d240f1eb6e40d5ec6d1b8b65
𓂺 <gagan@minbox.in>
2025-07-14 23:49:27 -0700

    binary search

--- src/numbers.rs
+++ src/numbers.rs
@@ -24,6 +24,30 @@ pub fn is_palindrome(number: i32) -> bool {
     true
 }
 
+pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+    if nums.is_empty() {
+        return -1;
+    }
+
+    let mut start = 0;
+    let mut end = nums.len() as isize -1;
+
+    while start <= end {
+        let mid = start + (end - start) / 2;
+        let mid_val = nums[mid as usize];
+
+        if mid_val == target {
+            return mid as i32;
+        }
+        if target > mid_val {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    -1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +55,12 @@ mod tests {
     fn is_palindrome_test() {
         assert_eq!(is_palindrome(121), true);
         assert_eq!(is_palindrome(122), false);
+    }
+
+    #[test]
+    fn test_search() {
+        assert_eq!(search(vec![1, 3, 4, 7, 8], 4), 2);
+        assert_eq!(search(vec![1, 3, 4, 7, 8], 5), -1);
+        assert_eq!(search(vec![5], -5), -1);
     }
 }
